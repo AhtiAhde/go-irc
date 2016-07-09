675ca77a0578abcc74368bd47d74652836188bc8
ThatGuyFromFinland <ahti.ahde@gmail.com>
2016-07-09 15:20:56 +0000

    Fix GetIP test

--- utils/ip_test.go
+++ utils/ip_test.go
@@ -4,6 +4,7 @@ import (
     "testing"
     "strconv"
     "unicode/utf8"
+    "strings"
 )
 
 func TestGetIP(t *testing.T) {
@@ -15,8 +16,11 @@ func TestGetIP(t *testing.T) {
 	for _, c := range cases {
 		got := GetIP()
 		length, _ := strconv.ParseInt(c.want, 10, 0)
-		if utf8.RuneCountInString(got) != int(length) {
-			t.Errorf("GetIP() returns a string of length %q", c.want)
+		if utf8.RuneCountInString(got) > int(length) {
+			t.Errorf("GetIP() returns a string longer (%q) than expected length %q", utf8.RuneCountInString(got), c.want)
+		}
+		if len(strings.Split(got, ".")) != 4 {
+		    t.Errorf("GetIP() does not return four digits separated by dots")
 		}
 	}
 }
